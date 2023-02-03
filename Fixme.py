4b90b0c20573582d551d283d69c49219d803ff7b
nethmin liyanage <nethmin.liyanage@gmail.com>
2023-02-03 11:44:50 -0800

    fixed formatting

--- Fixme.py
+++ Fixme.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def evens(n):
-    result = (list(filter(lambda x: x % 2 == 0, range(n+1))))
+    result = (list(filter(lambda x: x % 2 == 0, range(n + 1))))
     return result
